c412a7aebc5059b2cc85a1f4d284730f0406813b
junior-boop <juniorseppo3@gmail.com>
2024-04-16 12:02:12 +0100

    static

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,12 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-
 import tailwind from "@astrojs/tailwind";
+
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  output : "server",
-  site: 'https://example.com',
-  integrations: [mdx(), sitemap(), tailwind()]
+  output: "static",
+  integrations: [mdx(), sitemap(), tailwind(), react()]
 });
