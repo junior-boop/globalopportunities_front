a1fcc4fe90f14b9dc224f177618bc631d6f9706b
junior-boop <54283084+junior-boop@users.noreply.github.com>
2024-04-16 13:06:38 +0100

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-  output: "static",
+  output: "hybrid",
   integrations: [mdx(), sitemap(), tailwind(), react()],
   adapter: vercel()
 });
